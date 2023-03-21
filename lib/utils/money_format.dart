54973e0e0d2349cc3e18fada71df164360612679
glitchboyl <lonelyliar8523@gmail.com>
2023-03-21 13:51:14 +0800

    fix: floating point number

--- lib/utils/money_format.dart
+++ lib/utils/money_format.dart
@@ -1,6 +1,6 @@
 String moneyFormat(double money) {
   if (money == 0) return '0.00';
-  List<String> expansion = (money * 100).toInt().toString().split('');
+  List<String> expansion = (money * 100).toStringAsFixed(0).split('');
   while (expansion.length < 3) {
     expansion.insert(0, '0');
   }
